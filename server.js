37ceeb13f65bc691c7c906912c8f4329d8e49dcd
Marcelo Melo <marcelosdm@gmail.com>
2018-03-18 14:44:59 -0300

    Add projects page to this webserver

--- server.js
+++ server.js
@@ -45,6 +45,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        welcomeMessage: 'This is the Projects Page'
+    });
+})
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Bad request!'
